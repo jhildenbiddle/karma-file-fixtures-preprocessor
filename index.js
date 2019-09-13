d7eb3126ddd290190835b1aca1605da7692668fc
John Hildenbiddle <john@hildenbiddle.com>
2019-09-12 23:00:46 -0500

    Fix files/preprocessorts pattern match warning

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var DEFAULTS = {
         return content;
     }
 };
-var FILENAME = 'karma-file-fixtures.js';
-var FILEPATH = path.join(__dirname, FILENAME);
+var FILE_NAME = 'karma-file-fixtures.js';
+var FILE_PATH = path.join(__dirname, FILE_NAME);
 
 
 // Functions
@@ -30,23 +30,25 @@ function fileFixtures(args, config, logger, basePath) {
     var output   = '';
     var settings = Object.assign({}, DEFAULTS, config.fileFixtures);
 
-    var GLOBALVAR = 'window.' + settings.globalName;
+    var CONFIG_FILE_PATTERNS = [].concat(config.files).map(item => item.pattern || item);
+    var GLOBAL_VAR           = 'window.' + settings.globalName;
 
-    // Add plugin-generated JavaScript as first served file
+    // Add plugin-generated JavaScript as first included <script>
     config.files.unshift({
-        pattern : FILEPATH,
+        pattern : FILE_PATH,
         included: true,
         served  : true,
         watched : true
     });
 
-    // Add file paths associated with plugin
-    for (const path in config.preprocessors) {
-        const isFileFixturePath = config.preprocessors[path].indexOf('file-fixtures') !== -1;
+    // Add file paths associated with plugin to config.files array
+    for (const key in config.preprocessors) {
+        const isFileFixturePath = config.preprocessors[key].indexOf('file-fixtures') !== -1;
+        const isInFilePatterns  = CONFIG_FILE_PATTERNS.indexOf(key);
 
-        if (isFileFixturePath) {
+        if (isFileFixturePath && !isInFilePatterns) {
             config.files.unshift({
-                pattern : path,
+                pattern : key,
                 included: false,
                 served  : false,
                 watched : true
@@ -54,7 +56,7 @@ function fileFixtures(args, config, logger, basePath) {
         }
     }
 
-    output += util.format('%s = %s || {};\n', GLOBALVAR, GLOBALVAR);
+    output += util.format('%s = %s || {};\n', GLOBAL_VAR, GLOBAL_VAR);
 
     return function(content, file, done) {
         var basePath = path.posix.normalize(config.basePath + '/');
@@ -64,14 +66,14 @@ function fileFixtures(args, config, logger, basePath) {
 
         filePath = settings.transformKey(filePath) || filePath;
 
-        var key = util.format('%s[\'%s\']', GLOBALVAR, filePath);
+        var key = util.format('%s[\'%s\']', GLOBAL_VAR, filePath);
 
         if (output.indexOf(key) === -1) {
             log.debug('Processing', file.originalPath);
             content = settings.transformContent(filePath, content) || content;
             content = content.replace(/\r/g, '\\r').replace(/\n/g, '\\n');
             output += util.format('\n%s = \'%s\';\n', key, content);
-            fs.writeFileSync(FILEPATH, output);
+            fs.writeFileSync(FILE_PATH, output);
         }
 
         done(content);
@@ -83,7 +85,7 @@ fileFixtures.$inject = ['args', 'config', 'logger', 'config.basePath'];
 
 // Init
 // =============================================================================
-fs.writeFileSync(FILEPATH, '');
+fs.writeFileSync(FILE_PATH, '');
 
 
 // Export
